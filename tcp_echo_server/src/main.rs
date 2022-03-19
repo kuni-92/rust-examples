618875d71a9c52c8606b9ee81e5e2850b8ab7976
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2022-03-19 22:06:14 +0900

    update tcp_echo_server

--- tcp_echo_server/src/main.rs
+++ tcp_echo_server/src/main.rs
@@ -1,11 +1,8 @@
 use std::{env, io::{Read, Write}, net::{TcpListener, TcpStream}};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let addr = args[1].clone();
-    println!("address is {}", addr);
-    let port = args[2].clone();
-    let port = format!("{}:{}",addr, port);
+    let setting = check_args();
+    let port = format!("{}:{}",setting.addr, setting.port);
 
     println!("Start server!!!");
     println!("The port number to bind is {}", port);
@@ -14,6 +11,22 @@ fn main() {
     let (stream, _) = listener.accept().expect("Accept error.");
     loop {
         handle_echo(&stream);
+    }
+}
+
+struct ComSetting {
+    addr: String,
+    port: String,
+}
+
+fn check_args() -> ComSetting {
+    let args: Vec<String> = env::args().collect();
+    let addr = args[1].clone();
+    let port = args[2].clone();
+
+    ComSetting {
+        addr,
+        port,
     }
 }
 
